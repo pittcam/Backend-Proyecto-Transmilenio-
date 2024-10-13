2e9346c3ba9c087d2916ffb44de92881fb0e0e10
Juando26030 <juando02603@gmail.com>
2024-10-13 18:25:22 -0500

    RUTAS CONTROLADORES SIN API

--- transmilenio/src/main/java/com/co/transmilenio/controllers/AsignacionController.java
+++ transmilenio/src/main/java/com/co/transmilenio/controllers/AsignacionController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/asignaciones")
+@RequestMapping("/asignaciones")
 public class AsignacionController {
 
     @Autowired
--- transmilenio/src/main/java/com/co/transmilenio/controllers/BusController.java
+++ transmilenio/src/main/java/com/co/transmilenio/controllers/BusController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/buses")
+@RequestMapping("/buses")
 public class BusController {
 
     @Autowired
